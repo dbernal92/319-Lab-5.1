9642fb7ae032334b742b294642ce2749a76a6ac5
Darien Bernal <darien.bernal@gmail.com>
2025-03-17 18:23:00 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+
+import router from "./routes/grades.js";
+
+const PORT = process.env.PORT || 5050;
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the API.");
+});
+
+app.use('/grades', router)
+
+// Global error handling
+app.use((err, _req, res, next) => {
+  res.status(500).send("Seems like we messed up somewhere...");
+});
+
+// Start the Express server
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+});
